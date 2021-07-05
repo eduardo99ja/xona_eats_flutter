6164fc8f6ea57fc8da1d5e75bdc400c0fc553238
Apodaca <eduardo1ja99@gmail.com>
2021-07-05 14:27:02 -0500

    feat: Login page

--- lib/src/login/login_page.dart
+++ lib/src/login/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:xona_eats/src/utils/my_colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,9 +13,131 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola'),
+      body: Container(
+        width: double.infinity, //Todo el ancho de la pantalla
+        child: Stack(
+          children: [
+            Positioned(
+              top: -80,
+              left: -100,
+              child: _circleLogin(),
+            ),
+            Positioned(
+              top: 60.0,
+              left: 25.0,
+              child: _textLogin(),
+            ),
+            Column(
+              children: [
+                _imageBanner(),
+                _textFieldEmail(),
+                _textFieldPassword(),
+                _buttonLogin(),
+                _textDontHaveAccount(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  Widget _circleLogin() => Container(
+        width: 240.0,
+        height: 230.0,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(100.0), color: MyColors.primaryColor),
+      );
+
+  Widget _textLogin() => Text(
+        'LOGIN',
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 22.0),
+      );
+
+  Widget _imageBanner() => Container(
+        margin: EdgeInsets.only(top: 100.0, bottom: MediaQuery.of(context).size.height * 0.2),
+        child: Image.asset(
+          'assets/img/delivery.png',
+          width: 200.0,
+          height: 200.0,
+        ),
+      );
+
+  Widget _textFieldEmail() => Container(
+        margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 5.0),
+        decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'Correo electrónico',
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(15),
+            hintStyle: TextStyle(color: MyColors.primaryColorDark),
+            prefixIcon: Icon(
+              Icons.email,
+              color: MyColors.primaryColor,
+            ),
+          ),
+        ),
+      );
+
+  Widget _textFieldPassword() => Container(
+        margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 5.0),
+        decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+            hintText: 'Contraseña',
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(15),
+            hintStyle: TextStyle(color: MyColors.primaryColorDark),
+            prefixIcon: Icon(
+              Icons.lock,
+              color: MyColors.primaryColor,
+            ),
+          ),
+        ),
+      );
+
+  Widget _buttonLogin() => Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 30.0),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text('Ingresar'),
+          style: ElevatedButton.styleFrom(
+            primary: MyColors.primaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 15.0),
+          ),
+        ),
+      );
+
+  Widget _textDontHaveAccount() => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '¿No tienes cuenta?',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: MyColors.primaryColor,
+            ),
+          ),
+          SizedBox(
+            width: 7.0,
+          ),
+          Text(
+            'Registrate',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: MyColors.primaryColor,
+            ),
+          ),
+        ],
+      );
 }
