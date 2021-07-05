37b3a3f988862013d5c4e89b413ec8f5529fd4b0
Apodaca <eduardo1ja99@gmail.com>
2021-07-05 12:55:44 -0500

    feat: Set default primary color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:xona_eats/src/login/login_page.dart';
+import 'package:xona_eats/src/utils/my_colors.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,6 +23,7 @@ class _MyAppState extends State<MyApp> {
       routes: {
         'login': (BuildContext context) => LoginPage(),
       },
+      theme: ThemeData(primaryColor: MyColors.primaryColor),
     );
   }
 }
