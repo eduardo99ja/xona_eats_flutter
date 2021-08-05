8efeb44d46bcbefb571cb242718227deb442fb5d
Apodaca <eduardo1ja99@gmail.com>
2021-08-05 13:13:02 -0500

    feat: Register Fields validations

--- lib/src/pages/register/register_controller.dart
+++ lib/src/pages/register/register_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:xona_eats/src/models/response_api.dart';
 import 'package:xona_eats/src/models/user.dart';
 import 'package:xona_eats/src/provider/users_provider.dart';
+import 'package:xona_eats/src/utils/my_snackbar.dart';
 
 class RegisterController {
   BuildContext? context;
@@ -27,18 +28,30 @@ class RegisterController {
     String phone = phoneController.text.trim();
     String password = passwordController.text.trim();
     String confirmPassword = confirmPasswordController.text.trim();
-
+    // Validate not empty fields
+    if (email.isEmpty ||
+        name.isEmpty ||
+        lastname.isEmpty ||
+        phone.isEmpty ||
+        password.isEmpty ||
+        confirmPassword.isEmpty) {
+      MySnackbar.show(context!, 'Favor de ingresar todos los campos');
+      return;
+    }
+    //Validate passwords match
+    if (confirmPassword != password) {
+      MySnackbar.show(context!, 'Las contraseñas deben coincidir');
+      return;
+    }
+    //Validate password lenght
+    if (password.length < 6) {
+      MySnackbar.show(context!, 'La contraseña debe contener al menos 6 caracteres');
+      return;
+    }
     User user = User(email: email, name: name, lastname: lastname, phone: phone, password: password);
 
     ResponseApi? responseApi = await usersProvider.create(user);
 
-    print('Respuesta ${responseApi!.toJson()}');
-
-    print(email);
-    print(name);
-    print(lastname);
-    print(phone);
-    print(password);
-    print(confirmPassword);
+    MySnackbar.show(context!, responseApi!.message!);
   }
 }
---
+++ lib/src/utils/my_snackbar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MySnackbar {
+  static void show(BuildContext context, String text) {
+    FocusScope.of(context).requestFocus(new FocusNode());
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 14.0,
+        ),
+      ),
+      backgroundColor: Colors.black,
+      duration: Duration(seconds: 3),
+    ));
+  }
+}
